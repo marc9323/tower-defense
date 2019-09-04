b0c7a4f999340c1ddf09202ff7f98128bf121703
unknown <marcholman9323@gmail.com>
2019-09-04 13:56:05 -0500

    built progress bar

--- src/scenes/PreloaderScene.js
+++ src/scenes/PreloaderScene.js
@@ -6,6 +6,94 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   preload() {
+
+    // use fluid positioning for asset placement
+    // and sizing of game, base off main cameras width/height
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+
+    // add logo image
+    this.add.image(width / 2, height / 2 - 100, 'logo');
+
+    // display progress bar, loading bar and box around it
+    // to look like the box is filling up
+    var progressBar = this.add.graphics();
+    var progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(width / 2 - 160, height / 2 - 30, 320, 50);
+
+    // loading text
+    var loadingText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 50,
+        text: 'Loading...',
+        style: {
+            font: '20px monospace',
+            fill: '#ffffff'
+        }
+    });
+
+    // default origin of text is upper right hand corner
+    // set origin point to center to center text
+    loadingText.setOrigin(.5);
+
+    // percent text
+    var percentText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 5,
+        text: '0%',
+        style: {
+            font: '18px monospace',
+            fill: '#ffffff'
+        }
+    });
+    percentText.setOrigin(.5);
+
+    // loading assets text, displays name
+    // of file currently loading
+    var assetText = this.make.text({
+        x: width / 2,
+        y: height / 2 + 50,
+        text: 'asdf',
+        style: {
+            font: '18px monospace',
+            fill: '#ffffff'
+        }
+    });
+    assetText.setOrigin(.5);
+
+    // update progress bar
+
+    // listen for events progress and fileprogress
+    this.load.on('progress', (value) => {
+        // parse as int, whole numbers
+        percentText.setText(parseInt(value * 100) + '%');
+        // constantly clear what has been drawn to make
+        // it look like p bar loading
+        progressBar.clear();
+        progressBar.fillStyle(0xffffff, 1);
+        // inner rectangle inside progressBox
+        // draws each percentage increment up to 300 width
+        progressBar.fillRect(width / 2 - 150, height / 2 - 20, 300 * value, 30);
+        
+    });
+
+    // update file progress text
+    this.load.on('fileprogress', (file) => {
+        assetText.setText('Loading asset: ' + file.key);
+    });
+
+    // remove progress bar on complete, destroy components
+    this.load.on('complete', () => {
+        progressBox.destroy();
+        progressBar.destroy();
+        assetText.destroy();
+        percentText.destroy();
+        loadingText.destroy();
+    });
+
+
+
       // load asset images
     this.load.image('logo', 'assets/logo.png');
     this.load.image('bullet', 'assets/level/bulletDark2_outline.png');
@@ -20,6 +108,11 @@ export default class PreloaderScene extends Phaser.Scene {
     // placeholder 
     this.load.image('logo2', 'assets/logo.png');
 
+    // for testing progress bar
+    // for(var i = 0; i < 500; i++) {
+    //     this.load.image('logo2'+i, 'assets/logo.png');
+    // }
+
     // load asset tilemap in JSON format
     this.load.tilemapTiledJSON('level1', 'assets/level/level1.json');
     // this key needs to match that used in Tiled
@@ -28,13 +121,26 @@ export default class PreloaderScene extends Phaser.Scene {
         frameHeight: 64
     });
 
-
-
-
-    
   }
 
   create() {
-    this.scene.start('Game');
+    //this.scene.start('Game');
   }
 }
+
+
+
+/*
+NOTES:
+
+Loading Assets:
+1.) When loading assets Phaser will emit the following events:
+progress, fileprogress, and complete
+
+2.)  When progress event fires, it also provides a number between
+0 and 1 to inform you of the overall progress
+
+3.)  The complete event fires once all of the assets have 
+been loaded
+
+*/
